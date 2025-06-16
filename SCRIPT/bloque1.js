ceb741dc646706982ea78a397dd5170d1c47eac0
JuanD1033 <juand.her01@gmail.com>
2025-06-16 10:10:56 -0600

    avances Dana

---
+++ SCRIPT/bloque1.js
@@ -0,0 +1,8 @@
+function volver() {
+  window.history.back();
+}
+
+// Puedes agregar funciones interactivas para los temas si lo deseas
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Página del Bloque 1 cargada correctamente.");
+});
